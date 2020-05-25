2129225b5bf4345987c2f6091c2553ec5eb66fb6
sunliang <462028476@qq.com>
2020-05-25 21:39:00 +0800

    线程池配置

--- java-base/src/main/java/com/example/javabase/threadpool/MyThreadPoolDemo.java
+++ java-base/src/main/java/com/example/javabase/threadpool/MyThreadPoolDemo.java
@@ -1,9 +1,6 @@
 package com.example.javabase.threadpool;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * @author sunliang
@@ -14,6 +11,38 @@ import java.util.concurrent.TimeUnit;
 public class MyThreadPoolDemo
 {
     public static void main(String[] args)
+    {
+        ExecutorService threadPool = new ThreadPoolExecutor(2, 5, 1L,
+                TimeUnit.SECONDS,new LinkedBlockingDeque<>(3), Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.DiscardOldestPolicy());
+
+        try
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                threadPool.execute(() ->
+                {
+                    System.out.println(Thread.currentThread().getName() + "\t 办理业务");
+                });
+
+                //                try
+                //                {
+                //                    TimeUnit.MILLISECONDS.sleep(200);
+                //                } catch (Exception e)
+                //                {
+                //                    e.printStackTrace();
+                //                }
+            }
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        } finally
+        {
+            threadPool.shutdown();
+        }
+    }
+
+    private static void threadPoolInit()
     {
         //        Executor ExecutorS
         //        ExecutorService threadPool = Executors.newFixedThreadPool(5);//一池5个处理线程
@@ -46,6 +75,5 @@ public class MyThreadPoolDemo
         {
             threadPool.shutdown();
         }
-
     }
 }
